a5e67ba14196a61b7311f01a2ec5749909040f2b
AshirAzeem-Developer <ashir.dzynr@gmail.com>
2025-04-18 10:41:28 +0500

    FIxed Light and Dark Mode On My PC

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -31,7 +31,7 @@ const colorsLight = {
   isDark: false,
   BACKGROUND: '#FFFFFF', // White background
   PRIMARY_BACKGROUND: '#F6F9FE', // Soft blue-gray background (if needed)
-  TEXT: '#ffffff', // Black text
+  TEXT: '#000000', // Black text
   LIGHT_GRAY: '#C1C7C2', // Light gray for borders, separators
   GRAY: 'grey',
 };
